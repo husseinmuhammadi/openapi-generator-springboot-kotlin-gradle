2bd213c9fbb97187ebd139dfc7d385ffa4862568
Hossein Mohammadi <hossein.mohammadi@al.nl>
2023-03-21 12:13:08 +0100

    Add query parameters to get all endpoint

--- src/main/kotlin/com/javastudio/openapi/web/resources/OrderUnitsResource.kt
+++ src/main/kotlin/com/javastudio/openapi/web/resources/OrderUnitsResource.kt
@@ -1,6 +1,7 @@
 package com.javastudio.openapi.web.resources
 
 import com.javastudio.openapi.generated.v1.api.OrderUnitsApi
+import com.javastudio.openapi.generated.v1.model.OrderUnit
 import com.javastudio.openapi.generated.v1.model.StoreOrderUnit
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
@@ -9,15 +10,19 @@ import java.time.LocalDate
 @RestController
 class OrderUnitsResource : OrderUnitsApi {
 
-    override fun findAllOrderUnits(): ResponseEntity<List<StoreOrderUnit>> {
-        return super.findAllOrderUnits()
+    override fun findAllOrderUnits(
+        retailItemId: Long?,
+        supplierId: Long?,
+        depotId: Long?
+    ): ResponseEntity<List<OrderUnit>> {
+        return super.findAllOrderUnits(retailItemId, supplierId, depotId)
     }
 
     override fun findOrderUnit(
         orderUnitId: Int,
         storeGroupId: String?,
         refDate: LocalDate?
-    ): ResponseEntity<StoreOrderUnit> {
+    ): ResponseEntity<OrderUnit> {
         return super.findOrderUnit(orderUnitId, storeGroupId, refDate)
     }
 }
